d4b83896792bfbb19ef94d5e90824a206aa3d869
pythonrasmin <135885783+pythonrasmin@users.noreply.github.com>
2023-07-28 01:32:54 -0600

    Add files via upload

---
+++ wp_func.py
@@ -0,0 +1,25 @@
+def wp_p(p_text):
+    code = f"<!-- wp:paragraph --><p>{p_text}</p><!-- /wp:paragraph -->"
+    return code
+
+def wp_h2(heading2):
+    code = f"<!-- wp:heading --><h2>{heading2}</h2><!-- /wp:heading -->"
+    return code
+
+
+def wp_h3(heading3):
+    code = f"<!-- wp:heading --><h3>{heading3}</h3><!-- /wp:heading -->"
+    return code
+def wp_h4(heading4):
+    code = f"<!-- wp:heading --><h4>{heading4}</h4><!-- /wp:heading -->"
+    return code
+def wp_h5(heading5):
+    code = f"<!-- wp:heading --><h5>{heading5}</h5><!-- /wp:heading -->"
+    return code
+def wp_h6(heading6):
+    code = f"<!-- wp:heading --><h6>{heading6}</h6><!-- /wp:heading -->"
+    return code
+
+
+
+
